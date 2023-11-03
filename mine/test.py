4559875cdfcb90a9dc1a13e180a1fc6e85395388
br0wnboi <akukreja987@gmail.com>
2023-11-03 10:50:29 +0530

    fixed url

--- mine/test.py
+++ mine/test.py
@@ -14,13 +14,17 @@ date_with_extension = current_date + ".zip"
 encoded_date = base64.urlsafe_b64encode(date_with_extension.encode()).decode()
 
 # Append the encoded date to the URL
-url = f"https://www.whoisds.com//whois-database/newly-registered-domains/{encoded_date}/nrd"
+url = f"https://www.whoisds.com/whois-database/newly-registered-domains/{encoded_date}/nrd"
+
+# Print url
+print(url)
 
 # Download the zip file to the current directory
-urllib.request.urlretrieve(url, date_with_extension)
+urllib.request.urlretrieve(url)
 
 # Extract the contents of the zip file to a new folder in the current working directory with the name as the current date
-with zipfile.ZipFile(date_with_extension, 'r') as zip_ref:
-    os.mkdir(current_date)
-    zip_ref.extractall(current_date)
+# with zipfile.ZipFile(date_with_extension, 'r') as zip_ref:
+#    os.mkdir(current_date)
+#     zip_ref.extractall(current_date)
+
 
