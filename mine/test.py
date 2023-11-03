8eec9cd2487567a6210806d1677967521934ccde
br0wnboi <akukreja987@gmail.com>
2023-11-03 11:02:22 +0530

    fixed date formatting and URL

--- mine/test.py
+++ mine/test.py
@@ -3,15 +3,19 @@ import base64
 import urllib.request
 import zipfile
 import os
+import datetime
 
-# Get the current date in DD-MM-YY format
-current_date = datetime.datetime.now().strftime('%d-%m-%y')
+yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+yesterday_date = yesterday.strftime('%Y-%m-%d') 
+print(yesterday_date)
 
 # Append ".zip" to the date
-date_with_extension = current_date + ".zip"
+date_with_extension = yesterday_date + ".zip"
 
 # Encode the string in base64
-encoded_date = base64.urlsafe_b64encode(date_with_extension.encode()).decode()
+encoded_date = base64.b64encode(date_with_extension.encode()).decode()
+
+print(encoded_date)
 
 # Append the encoded date to the URL
 url = f"https://www.whoisds.com/whois-database/newly-registered-domains/{encoded_date}/nrd"
