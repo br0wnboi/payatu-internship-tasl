5b411960497e60b7222f3f6d3c5ccc29898b3f1e
br0wnboi <akukreja987@gmail.com>
2023-11-03 11:07:22 +0530

    fixes in url

--- mine/test.py
+++ mine/test.py
@@ -10,10 +10,10 @@ yesterday_date = yesterday.strftime('%Y-%m-%d')
 print(yesterday_date)
 
 # Append ".zip" to the date
-date_with_extension = yesterday_date + ".zip"
+yesterday_date_with_extension = yesterday_date + ".zip"
 
 # Encode the string in base64
-encoded_date = base64.b64encode(date_with_extension.encode()).decode()
+encoded_date = base64.b64encode(yesterday_date_with_extension.encode()).decode()
 
 print(encoded_date)
 
@@ -26,9 +26,12 @@ print(url)
 # Download the zip file to the current directory
 urllib.request.urlretrieve(url)
 
+# Save the response the from the URL and wait until it is downloaded
+response = urllib.request.urlopen(url)
+
 # Extract the contents of the zip file to a new folder in the current working directory with the name as the current date
-# with zipfile.ZipFile(date_with_extension, 'r') as zip_ref:
-#    os.mkdir(current_date)
-#     zip_ref.extractall(current_date)
+# with zipfile.ZipFile(yesterday_date_with_extension, 'r') as zip_ref:
+#    os.mkdir(yesterday_date)
+#   zip_ref.extractall()
 
 
